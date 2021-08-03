dfa5234f45f95c788ec3b170845e1aa5aee98823
Vikri Permana <vikripermana91@gmail.com>
2021-08-03 14:52:08 +0700

    finish all component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Icon, InlineIcon } from '@iconify/react';
 import wavingHand from '@iconify-icons/emojione/waving-hand';
 import downloadIcon from '@iconify-icons/akar-icons/download';
 import Typed from 'react-typed';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const Container = styled.div`
 width:100%;
 height:100%;
+background:#FFD60A;
 `
 const HeroSection = styled.div`
 width:100vw;
@@ -126,6 +128,7 @@ div.teks-wrap{
         margin-left:60px;
         color:#C55218;
         font-size:4rem;
+        -webkit-text-stroke: 2px black;
     }
     p{
         font-family: 'Overpass Mono', monospace;
@@ -134,6 +137,73 @@ div.teks-wrap{
 }
 `
 
+const ProjectSection = styled.div`
+height:100%;
+width:100%;
+background:#FFD60A;
+display:flex;
+align-items:center;
+padding:100px 0px;
+flex-direction:column;
+
+h1.title{
+    font-family: 'Bungee', cursive;
+    margin-left:60px;
+    color:#C55218;
+    font-size:8rem;
+    -webkit-text-stroke: 1px black;
+}
+div.project{
+width:90%;
+display:flex;
+justify-content:space-evenly;
+flex-wrap:wrap;
+}
+`
+
+const Kotak = styled.div`
+height:400px;
+width:400px;
+background:#E4B50B;
+margin-top:40px;
+transition:1s;
+display:flex;
+justify-content:flex-end;
+flex-direction:column;
+padding: 10px 20px;
+
+&:hover{
+    box-shadow:-30px 30px;
+    cursor:pointer;
+}
+
+h1{
+    font-family: 'Overpass Mono', monospace;
+        font-size:1.7rem
+}
+`
+
+const EndSection = styled.div`
+height:100%;
+width:100%;
+background:#FFD60A;
+padding:100px 10px 10px 10px;
+
+div.container{
+    width:90%;
+    display:flex;
+justify-content:space-between;
+}
+
+h1{
+    font-family: 'Overpass Mono', monospace;
+    font-size:1.7rem;
+}
+p{
+    font-family: 'Overpass Mono', monospace;
+    font-size:1.4rem;
+}
+`
 
 function App() {
 
@@ -143,11 +213,16 @@ function App() {
             <Container>
                 <HeroSection>
                     <div className="teks">
-                        <p className="animate__animated animate__fadeInLeftBig animate__delay-1s">hello there <Icon icon={wavingHand} /> i'm</p>
-                        <h1 className="animate__animated animate__fadeInDownBig animate__slow">VIKRI</h1>
+                        <ScrollAnimation animateIn="fadeInLeftBig">
+                            <p>hello there <Icon icon={wavingHand} /> i'm</p>
+                        </ScrollAnimation>
+
+                        <ScrollAnimation animateIn="fadeInDown">
+                            <h1>VIKRI</h1>
+                        </ScrollAnimation>
                         <div className="role">
                             <Typed
-                                className="subteks animate__animated animate__fadeIn animate__slower animate__delay-0.7s"
+                                className="subteks"
                                 strings={[
                                     'front end developer',
                                     'web designer',
@@ -155,44 +230,90 @@ function App() {
                                 ]}
                                 typeSpeed={50}
                                 backSpeed={50}
-                                startDelay={1200}
+                                startDelay={1500}
                                 loop
                             />
                             <br />
                         </div>
                     </div>
-                    <div className="button">
-                        <Button className="large">
-                            <span class="front">
-                                <Icon icon={downloadIcon} /> resume
-                            </span>
-                        </Button>
-                    </div>
+                    <ScrollAnimation animateIn="fadeIn" duration={1} delay={1200}>
+                        <div className="button">
+                            <Button className="large">
+                                <span class="front">
+                                    <Icon icon={downloadIcon} /> resume
+                                </span>
+                            </Button>
+                        </div>
+                    </ScrollAnimation>
                 </HeroSection>
                 <AboutSection>
                     <div className="about-wrapper">
-                        <div className="column-1">
-                            <img src="assets/photo/profile.jpeg" alt="" />
-                        </div>
+                        <ScrollAnimation animateIn="fadeIn">
+                            <div className="column-1">
+                                <img src="assets/photo/profile.jpeg" alt="" />
+                            </div>
+                        </ScrollAnimation>
                         <div className="column-2">
                             <div className="teks-wrap">
-                                <h1>About Me</h1>
+                                <ScrollAnimation animateIn="fadeInUp">
+                                    <h1>About Me</h1>
+                                </ScrollAnimation>
                                 <br />
-                                <p>Hello my name is Vikri Permana , you can call me vikri or whatever you want to
-                                    <br /> <br />
-                                    I am passionate on Design and Web Development
-                                    <br /> <br />
-                                    I am just graduated at High School , as Software Engineer Student
-                                    <br /><br />
-                                    I like listening to some music recently , cause its keeps my mood stable :D
-                                    <br /><br /><br /><br /><br />
-                                    Education Journey : <br />
-                                    SDN SUMBER SARI INDAH > SMPN 39 BANDUNG > SMKN 11 BANDUNG
-                                </p>
+                                <ScrollAnimation animateIn="fadeInUp" delay={500}>
+                                    <p>Hello my name is Vikri Permana , you can call me vikri or whatever you want to
+                                        <br /> <br />
+                                        I am passionate on Design and Web Development
+                                        <br /> <br />
+                                        I am just graduated at High School , as Software Engineer Student
+                                        <br /><br />
+                                        I like listening to some music recently , cause its keeps my mood stable :D
+                                        <br /><br /><br /><br /><br />
+                                        Education Journey : <br />
+                                        SDN SUMBER SARI INDAH &gt; SMPN 39 BANDUNG &gt; SMKN 11 BANDUNG
+                                    </p>
+                                </ScrollAnimation>
                             </div>
                         </div>
                     </div>
                 </AboutSection>
+                <ProjectSection>
+                    <ScrollAnimation animateIn="bounceInDown">
+                        <h1 className="animate__animated animate__pulse animate__slower animate__infinite title">PROJECTS</h1>
+                    </ScrollAnimation>
+                    <div className="project">
+                        <ScrollAnimation animateIn="fadeIn" delay={1000}>
+                            <Kotak>
+                                <h1>supervise</h1>
+                                <p>REACT JS + EXPRESS JS</p>
+                            </Kotak>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn="fadeIn" delay={1300}>
+                            <Kotak>
+                                <h1>monev bankeu</h1>
+                                <p>LARAVEL</p>
+                            </Kotak>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn="fadeIn" delay={1600}>
+                            <Kotak>
+                                <h1>real estate</h1>
+                                <p>REACT JS</p>
+                            </Kotak>
+                        </ScrollAnimation>
+                    </div>
+                </ProjectSection>
+                <EndSection>
+                    <div className="container">
+                        <div>
+                            <h1>EMAIL -</h1>
+                            <p>vikripermana91@gmail.com</p>
+                        </div>
+                        <div>
+                            <h1>PHONE -</h1>
+                            <p>+(62)815-6393-6568</p>
+                        </div>
+                    </div>
+                    <p>&copy; 2021 vikri</p>
+                </EndSection>
             </Container>
         </>
     )
--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import "animate.css/animate.compat.css";
+import "animate.css/animate.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
