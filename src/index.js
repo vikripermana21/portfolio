0e4af105fb9a29554346436615cc5783d53df6b5
Vikri Permana <vikripermana91@gmail.com>
2021-08-03 15:39:14 +0700

    fix things

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  HashRouter,
 } from "react-router-dom";
 import "animate.css/animate.compat.css";
 import "animate.css/animate.min.css";
@@ -16,11 +17,9 @@ ReactDOM.render(
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-    <Router>
+    <Router basename={window.location.pathname || ''}>
       <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
+        <Route exact path="/" component={App} />
       </Switch>
     </Router>
   </React.StrictMode>,
